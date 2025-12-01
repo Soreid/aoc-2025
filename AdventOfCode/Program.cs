797f96d82180c815b932d58cf59bc95174313a8d
Samuel Daley <sdaley818@gmail.com>
2025-12-01 06:37:58 -0700

    set program entry point to read inputs.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.WriteLine("Welcome to the Advent of Code 2025!");
+Console.Write("Please enter the day you would like to see the solution for (1-12): ");
+Int32.TryParse(Console.ReadLine(), out int selection);
+
+switch (selection)
+{
+    case 1:
+        Console.WriteLine("Day not yet completed");
+        break;
+    case 2:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 3:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 4:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 5:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 6:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 7:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 8:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 9:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 10:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 11:
+        Console.WriteLine("Day not yet released");
+        break;
+    case 12:
+        Console.WriteLine("Day not yet released");
+        break;
+    default:
+        Console.WriteLine("Input not recognized.");
+        break;
+}

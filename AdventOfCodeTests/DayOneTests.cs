d5f698457af062a081503f29be26fde696cc853d
Samuel Daley <sdaley818@gmail.com>
2025-12-03 15:39:44 -0700

    Added XUnit test project

---
+++ AdventOfCodeTests/DayOneTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventDayOne.Models;
+
+namespace AdventOfCodeTests
+{
+    public class DayOneTests
+    {
+        [Theory]
+        [InlineData(new string[] { "L50" }, 1)]
+        [InlineData(new string[] { "R50" }, 1)]
+        public void LandOnZeroCountsOnce(string[] instructions, int target)
+        {
+            DialModel dial = new(50, 100);
+
+            foreach (string instruction in instructions)
+            {
+                InstructionModel i = new(instruction);
+                dial.Spin(i.TurnDistance);
+            }
+
+            Assert.Equal(dial.ZeroClicks, target);
+        }
+    }
+}

aabb25290d1b2068fdfa64a94c37d5bb8f63f476
Samuel Daley <sdaley818@gmail.com>
2025-12-03 21:46:18 -0700

    Created class and model template

---
+++ AdventDayTwo/DayTwo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDayTwo
+{
+    public class DayTwo(string input)
+    {
+        string[] Inputs { get; set; } = input.Split(',');
+    }
+}
---
+++ AdventDayTwo/Models/SectionModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace AdventDayTwo.Models
+{
+    internal class SectionModel(string range)
+    {
+        public int Start { get; private set; } = int.Parse(range.Split('-')[0]);
+        public int End { get; private set; } = int.Parse(range.Split('-')[1]);
+
+        public IEnumerable<int> GetValues()
+        {
+            for (int i = Start; i <= End; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}

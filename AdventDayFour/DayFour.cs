96519a97eab99039e5187fa9e54e14c68bc22171
Samuel Daley <sdaley818@gmail.com>
2025-12-05 14:10:27 -0700

    Added day four class and methods.

---
+++ AdventDayFour/DayFour.cs
@@ -0,0 +1,24 @@
+﻿using AdventDayFour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDayFour
+{
+    public class DayFour(string[] data)
+    {
+        internal GridModel Grid { get; private set; } = new(data);
+
+        internal bool IsAccessible(char[] blockers, int threshold, int radius)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetAccessibleCount(char[] blockers, int threshold, int radius)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventDayFour/Models/GridModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDayFour.Models
+{
+    internal class GridModel(string[] data)
+    {
+        internal char[,] Points { get; private set; }
+
+        internal char[] GetNeighbors(int[] pos, int radius)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

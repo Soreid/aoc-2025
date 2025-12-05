682ad0ab8c72ce898a3badf7caa74d82dcf9aac8
Samuel Daley <sdaley818@gmail.com>
2025-12-04 21:36:21 -0700

    Added Advent Day Three class and test files.

---
+++ AdventDayThree/DayThree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDayThree
+{
+    public class DayThree(string[] input)
+    {
+        public string FindLargestSet(string input, int start, int buffer)
+        {
+
+        }
+    }
+}
---
+++ AdventOfCodeTests/DayThreeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventDayThree; 
+
+namespace AdventOfCodeTests
+{
+    public class DayThreeTests
+    {
+        [Theory]
+        [InlineData("9876111", 2, "98")]
+        [InlineData("1211419", 2, "49")]
+        [InlineData("3852639618", 3, "968")]
+        public void GetsCorrectArrangement(string input, int buffer, string expected)
+        {
+            DayThree dayThree = new(new [] { "" });
+            string result = dayThree.FindLargestSet(input, 0, buffer);
+            Assert.Equal(expected, result);
+        }
+    }
+}

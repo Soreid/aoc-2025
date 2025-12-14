663a0665d05d8d6c52e2f004d81d87225e218688
Samuel Daley <sdaley818@gmail.com>
2025-12-14 10:05:57 -0700

    Created ProblemModel class for refactoring solution code

---
+++ AdventDaySix/Models/ProblemModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDaySix.Models
+{
+    internal class ProblemModel
+    {
+        public List<int> Numbers { get; private set; } = new();
+        public char Operator { get; private set; }
+        public long Calc()
+        {
+            long output = Numbers[0];
+
+            if (Operator == '+')
+            {
+                for (int i = 1; i < Numbers.Count; i++) 
+                {
+                    output += Numbers[i];
+                }
+            }
+
+            if (Operator == '*')
+            {
+                for (int i = 1; i < Numbers.Count; i++)
+                {
+                    output *= Numbers[i];
+                }
+            }
+
+            return output;
+        }
+    }
+}

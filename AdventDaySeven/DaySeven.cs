6f322b17edec4be7c952b9c0abfa44b0127abff4
Samuel Daley <sdaley818@gmail.com>
2025-12-14 13:47:48 -0700

    Added projects and references for day seven

---
+++ AdventDaySeven/DaySeven.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDaySeven
+{
+    public class DaySeven(string[] inputs)
+    {
+        public string[] Inputs { get; set; } = inputs;
+
+
+    }
+}

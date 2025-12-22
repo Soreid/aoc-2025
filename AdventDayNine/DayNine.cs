77f3197f4b45809aa9803367581dc2d147e6f43d
Samuel Daley <sdaley818@gmail.com>
2025-12-22 09:05:28 -0700

    Created project files and test cases for Day Nine

---
+++ AdventDayNine/DayNine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventDayNine
+{
+    public class DayNine
+    {
+        public int[,] RedTiles;
+        public DayNine(string[] inputs)
+        {
+            RedTiles = new int[inputs.Length,2];
+            for(int i = 0; i < inputs.Length; i++)
+            {
+                string[] strNums = inputs[i].Split(',');
+                RedTiles[i, 0] = int.Parse(strNums[0]);
+                RedTiles[i, 1] = int.Parse(strNums[1]);
+            }
+        }
+
+        public long GetLargestArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal long GetRectArea(int[,] corner1, int[,] corner2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

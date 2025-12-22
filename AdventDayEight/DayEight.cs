112418c63dbe9cb0b303aa759671266aa7dfc7ba
Samuel Daley <sdaley818@gmail.com>
2025-12-21 18:14:24 -0700

    Implemented helper functions to calculate and track junction boxes

--- AdventDayEight/DayEight.cs
+++ AdventDayEight/DayEight.cs
@@ -10,25 +10,51 @@ namespace AdventDayEight
     public class DayEight
     {
         internal JunctionModel[] Junctions { get; private set; }
+        internal Dictionary<int, int> CircuitSizes { get; private set; }
 
         public DayEight(string[] inputs)
+        {
+            Junctions = new JunctionModel[inputs.Length];
+            for(int i = 0; i < Junctions.Length; i++)
+            {
+                Junctions[i] = CreateJunction(inputs[i]);
+                Junctions[i].Circuit = i;
+            }
+        }
+
+        public long ConnectJunctions(int connections)
         {
             throw new NotImplementedException();
         }
 
         internal JunctionModel CreateJunction(string coordinates)
         {
-            throw new NotImplementedException();
+            string[] coords = coordinates.Split(',');
+            return new(long.Parse(coords[0]), long.Parse(coords[1]), long.Parse(coords[2]));
         }
 
-        internal double CalculateDistance(JunctionModel box1, JunctionModel box2)
-        {
-            throw new NotImplementedException();
-        }
+        internal double CalculateDistance(JunctionModel box1, JunctionModel box2) =>
+            Math.Sqrt(Math.Pow((box1.X - box2.X), 2) + 
+                      Math.Pow((box1.Y - box2.Y), 2) + 
+                      Math.Pow((box1.Z - box2.Z), 2));
 
         internal void ConnectJunctions(JunctionModel box1, JunctionModel box2)
         {
-            throw new NotImplementedException();
+            CircuitSizes.Remove(box2.Circuit);
+            int c = box1.Circuit;
+            JunctionModel[] additions = Junctions.Where(x => x.Circuit == box2.Circuit).ToArray();
+            foreach (JunctionModel addition in additions)
+            {
+                addition.Circuit = c;
+            }
+            if (CircuitSizes.ContainsValue(c))
+            {
+                CircuitSizes[c] += additions.Length;
+            }
+            else
+            {
+                CircuitSizes.Add(c, Junctions.Where(x => x.Circuit == c).Count());
+            }
         }
     }
 }
--- AdventOfCodeTests/DayEightTests.cs
+++ AdventOfCodeTests/DayEightTests.cs
@@ -13,7 +13,7 @@ namespace AdventOfCodeTests
         [Theory]
         [InlineData("3,4,5", "6,7,8", 5.1962)]
         [InlineData("1,1,1", "1,1,2", 1.0000)]
-        [InlineData("415,312,416", "813,218,582", 441.3569)]
+        [InlineData("415,312,416", "813,218,582", 441.3570)]
         public void ShortestDistanceIsCalculated(string box1, string box2, double expected)
         {
             DayEight dayEight = new([]);
